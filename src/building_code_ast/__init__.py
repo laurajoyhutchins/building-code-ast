0fad7f39889f11840c7b23a311180c99ee686fa0
Laura <laurajoyhutchins@gmail.com>
2026-08-04 20:53:43 -0600

    feat: export NFPA 13 bundle contracts

--- src/building_code_ast/__init__.py
+++ src/building_code_ast/__init__.py
@@ -12,6 +12,14 @@ from .document_model import (
 )
 from .document_validation import validate_document_ast
 from .model import ProvisionAst
+from .nfpa13_bundle import (
+    BUNDLE_SCHEMA as NFPA13_BUNDLE_SCHEMA,
+    PRODUCER_SCHEMA as NFPA13_PRODUCER_SCHEMA,
+    finalize_raw_nfpa13_bundle,
+    read_nfpa13_bundle,
+    validate_nfpa13_bundle_contract,
+    validate_review_registry,
+)
 from .parser import parse_provision
 from .validation import validate_ast
 
@@ -21,11 +29,17 @@ __all__ = [
     "DocumentNode",
     "DocumentNodeType",
     "DocumentSourceArtifact",
+    "NFPA13_BUNDLE_SCHEMA",
+    "NFPA13_PRODUCER_SCHEMA",
     "ProvisionAst",
     "document_ast_from_dict",
     "document_node_id",
+    "finalize_raw_nfpa13_bundle",
     "make_document_node",
     "parse_provision",
+    "read_nfpa13_bundle",
     "validate_ast",
     "validate_document_ast",
+    "validate_nfpa13_bundle_contract",
+    "validate_review_registry",
 ]
