ccd4679b8e7d890b8a377ffd71532d910dbbe5d1
Laura <laurajoyhutchins@gmail.com>
2026-08-09 09:55:57 -0600

    feat: add ACI page parser entry point

---
+++ src/building_code_ast/ingest/aci318.py
@@ -0,0 +1,9 @@
+"""ACI 318-19 publication-structure recognition."""
+
+from __future__ import annotations
+
+
+def parse_aci318_page(*args, **kwargs):
+    """Parse one ACI 318-19 page slice into Document AST structure."""
+
+    raise NotImplementedError("ACI 318-19 page parsing is not implemented yet")
